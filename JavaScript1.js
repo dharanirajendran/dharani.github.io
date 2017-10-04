cefb1ff96ba6149496fcaae1220fc5b62c358f04
DharaniRajendran <32294015+dharanirajendran@users.noreply.github.com>
2017-10-04 14:45:37 +0530

    new files

---
+++ JavaScript1.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.progress .progress-bar').css("width",
+              function () {
+                  return $(this).attr("aria-valuenow") + "%";
+              }
+      )
+});
